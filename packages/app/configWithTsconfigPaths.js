a4b08287ab49ddc16604b050aa0a58a263df6bb1
Fernando Terra <nandobrt@gmail.com>
2022-09-23 12:37:52 -0300

    feat: ensure proper module resolution
    Regardless of node command's cwd.

--- packages/app/configWithTsconfigPaths.js
+++ packages/app/configWithTsconfigPaths.js
@@ -4,4 +4,10 @@ const tsConfigPaths = require("tsconfig-paths");
 tsConfigPaths.register({
   baseUrl: tsConfig.compilerOptions.baseUrl,
   paths: tsConfig.compilerOptions.paths,
+
+  /**
+   * This is useful to ensure the module resolution works regardless of the directory node is run from.
+   * Note: in case node is run from this very directory, it works regardless of this parameter.
+   */
+  cwd: __dirname,
 });
